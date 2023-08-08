30feab040d6d39eca9236c66fe199f26c3377c11
Cassie Roan <cassandracroan@gmail.com>
2023-08-08 12:54:41 -0400

    Finished product

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function SignUpForm({ setToken }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {method: "POST",}) ;
+        const result = await response.json();
+        console.log(result);
+        setToken(result.token);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h1>Sign Up</h1>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+      { !(username.length >= 8) && <p style={{color: 'red'}}>
+            Username must be at least 8 characters
+        </p>
+      }
+    </>
+  );
+}
